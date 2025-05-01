854f6531c0b000f56c17246286f7628f48b9bb9c
damien13williams <190779546+damien13williams@users.noreply.github.com>
2025-05-01 14:19:06 -0500

    fixed form error and font

--- routes/pokemonRoutes.js
+++ routes/pokemonRoutes.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const { Pokemon, Trainer, Type } = require('../models');
 const axios = require('axios');
+const { Pokemon, Trainer, Type } = require('../models');
 
+// Show the form to add a new Pokémon
+router.get('/pokemons/add', async (req, res) => {
+  if (!req.session.trainerId) {
+    return res.redirect('/login'); // Ensure the trainer is logged in
+  }
+
+  const trainerId = req.session.trainerId;
+
+  try {
+    const types = await Type.findAll();
+    res.render('addPokemon', { trainerId, types, error: null });
+  } catch (error) {
+    console.error('Error fetching data for add Pokemon:', error);
+    res.status(500).send('Error fetching data for add Pokemon');
+  }
+});
 
 // Handle adding a new Pokémon
 router.post('/pokemons/add', async (req, res) => {
